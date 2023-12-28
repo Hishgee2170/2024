6baddceaa9f3bd1173bac3a7c441e13cde53a63d
Hishgee2170 <Hishgeehishge21@gmail.com>
2023-12-28 18:52:13 +0800

    za shaachih

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const myButton = [];
+myButton[0] = document.getElementById("mybutt1");
+myButton[1] = document.getElementById("mybutt2");
+myButton[2] = document.getElementById("mybutt3");
+myButton[3] = document.getElementById("mybutt4");
+
+// element nemeh function ()
+// function addCard() {
+//     let divAddCard = document.createElement("div");
+//     divAddCard.style.backgroundColor = "red";
+//     divAddCard.style.height = "100px";
+//     divAddCard.style.width = "100px";
+//     let bigFatherDiv = document.getElementById("box1");
+//     bigFatherDiv.appendChild(divAddCard);
+// }
+
+function addTaskShow() {
+    let TaskShow = document.getElementsByClassName("addTaskk")[0];
+    TaskShow.style.display = "flex";
+}
+
+// function hide() {
+//     let TaskShow = document.getElementsByClassName("addTaskk")[0];
+//     TaskShow.style.display = "none";
+// }
+myButton[0].addEventListener("click", addTaskShow);
